e270d07c222d9163e36592ed2a640a0dc08e423f
arfatS <arfatsarole@gmail.com>
2020-04-27 18:12:01 +0530

    rest api routes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 const express = require('express')
-
-const User = require('./models/user')
-const Task = require('./models/task')
-
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 require('./db/mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-//Parsing incoming JSON to Objects
 app.use(express.json())
-
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(()=>{
-        res.status(201).send(user)
-    }).catch((error)=>{
-        res.status(400).send(error)
-    })
-})
-
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(()=>{
-        res.status(201).send(task)
-    }).catch((error)=>{
-        res.status(400).send(error)
-    })
-
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running onn port ${port}`)
